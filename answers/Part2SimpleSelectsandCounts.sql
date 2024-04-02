6129d3df69201bdee860b6d3a72395062ec272ab
ShailyGH <shailykunal@.com>
2024-04-02 16:44:42 -0400

    Part1 and Part2 completed.

---
+++ answers/Part2SimpleSelectsandCounts.sql
@@ -0,0 +1,14 @@
+1. What are all the types of pokemon that a pokemon can have?
+select * from tyupes;
+
+2. What is the name of the pokemon with id 45?
+select name from pokemons where id = 45;
+
+3. How many pokemon are there?
+select count(*) as "count" from pokemons;
+
+4. How many types are there?
+select count(*) as "count" from types;
+
+5. How many pokemon have a secondary type?
+select count(secondary_type) as "count" from pokemons;
